7a7b712001d89e596f016b4983aa7042b0c84e97
Ashwin Phadke <ashwinphadke12@rediffmail.com>
2020-06-05 23:31:56 +0530

    - added prcess full script

--- flask_process.py
+++ flask_process.py
@@ -27,7 +27,27 @@ def convert_ret_tuple(tup):
     str = ''.join(tup)
     return str
 
-
+@app.route('/')
+def process_video():
+    # isf = True
+    # while isf:
+    conn = sqlite3.connect(
+        'db/cvplayground.sqlite')
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT id, location, model_name FROM uploads WHERE isProcessed=0 order by datetime DESC")
+    #cur.execute("SELECT id, location, model_name FROM uploads WHERE isProcessed=0 order by datetime DESC LIMIT 1")
+    id, location, model_name = cur.fetchone()
+    print(id, location, model_name)
+    if not (id, location):
+        cur.execute(
+        "SELECT id, location FROM uploads WHERE isProcessed=0 order by datetime DESC")
+    #objdetectionfunc(location, id, model_name)s
+    dectect_func(location, id, model_name)
+    cur.execute("UPDATE uploads SET isProcessed=1  WHERE id='"+id+"'")
+    conn.commit()
+    conn.close()
+    print('yes')
 
 
 # if __name__ == "__main__":
