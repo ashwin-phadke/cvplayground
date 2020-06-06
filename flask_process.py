5e8bd0dbb35ca5b7dec0431e695966e7c6ad9e67
Ashwin Phadke <ashwinphadke12@rediffmail.com>
2020-06-06 20:23:15 +0530

    - deployed script added

--- flask_process.py
+++ flask_process.py
@@ -2,15 +2,13 @@ import os
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 import os
-#import magic
 import urllib.request
 from subprocess import Popen
 #from webapp import app
 from flask import Flask, flash, request, redirect, render_template
 from werkzeug.utils import secure_filename
 from codes import video_objdet
-#from codes.video_objdet import objdetectionfunc
-from codes.temp_vid_objdet import dectect_func
+from codes.video_objdet import objdetectionfunc
 from pathlib import Path
 import uuid
 import sqlite3
@@ -29,8 +27,7 @@ def convert_ret_tuple(tup):
 
 @app.route('/')
 def process_video():
-    # isf = True
-    # while isf:
+
     conn = sqlite3.connect(
         'db/cvplayground.sqlite')
     cur = conn.cursor()
@@ -42,23 +39,9 @@ def process_video():
     if not (id, location):
         cur.execute(
         "SELECT id, location FROM uploads WHERE isProcessed=0 order by datetime DESC")
-    #objdetectionfunc(location, id, model_name)s
-    dectect_func(location, id, model_name)
+    objdetectionfunc(location, id, model_name)
     cur.execute("UPDATE uploads SET isProcessed=1  WHERE id='"+id+"'")
     conn.commit()
     conn.close()
-    print('yes')
 
 
-# if __name__ == "__main__":
-#     process_video()
-    # try :
-    #     #app.run(port=5555, debug=True)
-    #     process_video()
-    # except Exception as e:
-    #     print(e)
-        
-    # 
-
-
-    # process_video()
