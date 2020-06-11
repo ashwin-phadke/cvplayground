fb0ed32c554a763290f7982cdd43e893aa9f2af3
Ashwin Phadke <ashwinphadke12@rediffmail.com>
2020-06-11 23:28:37 +0530

    - added support for subprocess

--- flask_process.py
+++ flask_process.py
@@ -1,31 +1,18 @@
 import os
-from flask import Flask, flash, request, redirect, url_for
-from werkzeug.utils import secure_filename
 import os
-import urllib.request
 from subprocess import Popen
-#from webapp import app
-from flask import Flask, flash, request, redirect, render_template
-from werkzeug.utils import secure_filename
-from codes import video_objdet
 from codes.video_objdet import objdetectionfunc
 from pathlib import Path
 import uuid
 import sqlite3
-
-# UPLOAD_FOLDER = '/tensorflow/uploads'
-
-app = Flask(__name__)
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-app.secret_key = "secret key"
+from importlib import reload
 
 
 def convert_ret_tuple(tup):
     str = ''.join(tup)
     return str
 
-@app.route('/')
+
 def process_video():
 
     conn = sqlite3.connect(
@@ -35,7 +22,7 @@ def process_video():
         "SELECT id, location, model_name FROM uploads WHERE isProcessed=0 order by datetime DESC")
     #cur.execute("SELECT id, location, model_name FROM uploads WHERE isProcessed=0 order by datetime DESC LIMIT 1")
     id, location, model_name = cur.fetchone()
-    print(id, location, model_name)
+    #print(id, location, model_name)
     if not (id, location):
         cur.execute(
         "SELECT id, location FROM uploads WHERE isProcessed=0 order by datetime DESC")
@@ -43,5 +30,9 @@ def process_video():
     cur.execute("UPDATE uploads SET isProcessed=1  WHERE id='"+id+"'")
     conn.commit()
     conn.close()
+    return id
+    #reload(video_objdet)
 
 
+if __name__ == "__main__":
+    process_video()
