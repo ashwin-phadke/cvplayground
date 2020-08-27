d437c19c0d0f41fa93853951d6da9e2c3defc3c1
Ashwin Phadke <ashwinphadke12@rediffmail.com>
2020-08-27 20:06:39 +0530

    - updated comments and descriptions.

--- codes/model_downloader.py
+++ codes/model_downloader.py
@@ -1,11 +1,17 @@
-import tensorflow as tf
-import urllib
-import tarfile
+# Do necessary imports
 import os
+import tarfile
+import urllib
 from os import chdir
+from pathlib import Path
+
+import tensorflow as tf
 import wget
 
-from pathlib import Path
+
+# List of model files to download, this list comprises of the most common models used. To add your model simply go
+# to https://github.com/tensorflow/models/blob/archive/research/object_detection/g3doc/tf1_detection_zoo.md and select the name of your
+#appropriate model to download
 
 download_model_list = ["ssd_mobilenet_v2_coco_2018_03_29", "ssd_inception_v2_coco_2017_11_17",
  "faster_rcnn_inception_v2_coco_2018_01_28", "faster_rcnn_resnet50_coco",
@@ -13,7 +19,7 @@ download_model_list = ["ssd_mobilenet_v2_coco_2018_03_29", "ssd_inception_v2_coc
  "mask_rcnn_inception_resnet_v2_atrous_coco", "ssd_inception_v2_coco", "faster_rcnn_inception_v2_coco"]
 
 
-# Download and extract model
+# Download and extract model in the detect_models directory, to get the directory structure required for this project check README.
 def download_model(model_name):
     base_url = 'http://download.tensorflow.org/models/object_detection/'
     model_file = base_url + model_name +  '.tar.gz'
@@ -47,6 +53,3 @@ for model_name in download_model_list:
 
 # LABEL_FILENAME = 'mscoco_label_map.pbtxt'
 # PATH_TO_LABELS = download_labels(LABEL_FILENAME)
-
-
-
