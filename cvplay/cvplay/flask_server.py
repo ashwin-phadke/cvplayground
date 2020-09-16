48a689684cdbc360d97a7d7b3262641de553e254
Ashwin Phadke <ashwinphadke12@rediffmail.com>
2020-09-16 22:37:28 +0530

    - changes to file selection, added func for seg.

--- cvplay/cvplay/flask_server.py
+++ cvplay/cvplay/flask_server.py
@@ -38,6 +38,16 @@ model_dict = {"ssdmv2i": ("ssd_mobilenet_v2_coco_2018_03_29", "ssd_mobilenet_v2_
               "frcnnr50": ("faster_rcnn_resnet50_coco_2018_01_28", "faster_rcnn_resnet50_coco_2018_01_28.pbtxt"),
               "mrcnniv2": ("mask_rcnn_inception_v2_coco_2018_01_28", "mask_rcnn_inception_v2_coco_2018_01_28")}
 
+_MODEL_URLS = {
+    'mobilenetv2_coco_voctrainaug':
+        'deeplabv3_mnv2_pascal_train_aug_2018_01_29.tar.gz',
+    'mobilenetv2_coco_voctrainval':
+        'deeplabv3_mnv2_pascal_trainval_2018_01_29.tar.gz',
+    'xception_coco_voctrainaug':
+        'deeplabv3_pascal_train_aug_2018_01_04.tar.gz',
+    'xception_coco_voctrainval':
+        'deeplabv3_pascal_trainval_2018_01_04.tar.gz',
+}
 
 def generate_uuid():
     new_id = uuid.uuid4()
@@ -67,6 +77,55 @@ def upload_form():
 @app.route('/upload_page', methods=['GET', 'POST'])
 def show_form():
     return render_template('modelselectindex.html')
+
+@app.route('/segmentation_upload_page', methods=['GET', 'POST'])
+def show_segmentation_form():
+    return render_template('modelselectsegmentationindex.html')
+
+@app.route('upload_segmentation_page', methods=['POST', 'GET'])
+def upload_segmentation_file():
+    if request.method == 'POST':
+        ip_address = request.remote_addr
+        conn = sqlite3.connect('db/cvplayground.sqlite')
+        logging.info('User %s entered app', ip_address)
+        if 'file' not in request.files:
+            if 'model' not in request.form:
+                return redirect(request.url)
+        file = request.files['file']
+        backbone_model_name = request.form['radios']
+        download_model_name = _MODEL_URLS[backbone_model_name]
+
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            logging.info('User %s successfully saved file', ip_address)
+            print("File saved successfully")
+            cur = conn.cursor()
+            new_uuid = str(generate_uuid())
+
+            dtt = date_time()
+            isUploaded = True
+            isProcessed = False
+            location = file_path
+            status = 1
+            cur.execute("INSERT INTO uploads (id, status, isUploaded, isProcessed, location, datetime, model_name, pbtxt_name) values(?, ?, ?, ?, ?, ?, ?, ?)",
+                        (new_uuid, status, isUploaded, isProcessed, location, dtt, model_name, pbtxt_name))
+            conn.commit()
+            conn.close()
+            logging.info('File saved successfully from %s user', ip_address)
+            process_video()
+            filename = new_uuid + '.mp4'
+            time.sleep(5)
+            return redirect('/downloadfile/' + filename)
+
+        else:
+            flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
+            logging.info('User %s did not save a video file', ip_address)
+            return redirect(request.url)
+
+
 
 
 @app.route('/uploads', methods=['POST', 'GET'])
