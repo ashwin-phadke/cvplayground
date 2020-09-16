90e9614dbff42ceb62ef59e8657f7d7c57bfee82
Ashwin Phadke <ashwinphadke12@rediffmail.com>
2020-09-16 21:04:18 +0530

    - added preprocess as a function call.

--- cvplay/cvplay/deeplab_sem_seg.py
+++ cvplay/cvplay/deeplab_sem_seg.py
@@ -47,214 +47,223 @@ These methods help us perform the following tasks:
 * Visualize an image, and add an overlay of colors on various regions
 """
 
+
 class DeepLabModel(object):
-  """Class to load deeplab model and run inference."""
+    """Class to load deeplab model and run inference."""
 
-  INPUT_TENSOR_NAME = 'ImageTensor:0'
-  OUTPUT_TENSOR_NAME = 'SemanticPredictions:0'
-  INPUT_SIZE = 513
-  FROZEN_GRAPH_NAME = 'frozen_inference_graph'
+    INPUT_TENSOR_NAME = 'ImageTensor:0'
+    OUTPUT_TENSOR_NAME = 'SemanticPredictions:0'
+    INPUT_SIZE = 513
+    FROZEN_GRAPH_NAME = 'frozen_inference_graph'
 
-  def __init__(self, tarball_path):
-    """Creates and loads pretrained deeplab model."""
-    self.graph = tf.Graph()
+    def __init__(self, tarball_path):
+        """Creates and loads pretrained deeplab model."""
+        self.graph = tf.Graph()
 
-    graph_def = None
-    # Extract frozen graph from tar archive.
-    tar_file = tarfile.open(tarball_path)
-    for tar_info in tar_file.getmembers():
-      if self.FROZEN_GRAPH_NAME in os.path.basename(tar_info.name):
-        file_handle = tar_file.extractfile(tar_info)
-        #graph_def = tf.GraphDef.FromString(file_handle.read())
-        graph_def = tf1.GraphDef.FromString(file_handle.read())
-        break
+        graph_def = None
+        # Extract frozen graph from tar archive.
+        tar_file = tarfile.open(tarball_path)
+        for tar_info in tar_file.getmembers():
+            if self.FROZEN_GRAPH_NAME in os.path.basename(tar_info.name):
+                file_handle = tar_file.extractfile(tar_info)
+                #graph_def = tf.GraphDef.FromString(file_handle.read())
+                graph_def = tf1.GraphDef.FromString(file_handle.read())
+                break
 
-    tar_file.close()
+        tar_file.close()
 
-    if graph_def is None:
-      raise RuntimeError('Cannot find inference graph in tar archive.')
+        if graph_def is None:
+            raise RuntimeError('Cannot find inference graph in tar archive.')
 
-    with self.graph.as_default():
-      tf.import_graph_def(graph_def, name='')
+        with self.graph.as_default():
+            tf.import_graph_def(graph_def, name='')
 
-    #self.sess = tf.Session(graph=self.graph)
-    self.sess = tf1.Session(graph=self.graph)
+        self.sess = tf1.Session(graph=self.graph)
 
-  def run(self, image):
-    """Runs inference on a single image.
+    def run(self, image):
+        """Runs inference on a single image.
 
-    Args:
-      image: A PIL.Image object, raw input image.
+        Args:
+          image: A PIL.Image object, raw input image.
 
-    Returns:
-      resized_image: RGB image resized from original input image.
-      seg_map: Segmentation map of `resized_image`.
-    """
-    width, height = image.size
-    resize_ratio = 1.0 * self.INPUT_SIZE / max(width, height)
-    target_size = (int(resize_ratio * width), int(resize_ratio * height))
-    resized_image = image.convert('RGB').resize(target_size, Image.ANTIALIAS)
-    batch_seg_map = self.sess.run(
-        self.OUTPUT_TENSOR_NAME,
-        feed_dict={self.INPUT_TENSOR_NAME: [np.asarray(resized_image)]})
-    seg_map = batch_seg_map[0]
-    return resized_image, seg_map
+        Returns:
+          resized_image: RGB image resized from original input image.
+          seg_map: Segmentation map of `resized_image`.
+        """
+        width, height = image.size
+        resize_ratio = 1.0 * self.INPUT_SIZE / max(width, height)
+        target_size = (int(resize_ratio * width), int(resize_ratio * height))
+        resized_image = image.convert('RGB').resize(
+            target_size, Image.ANTIALIAS)
+        batch_seg_map = self.sess.run(
+            self.OUTPUT_TENSOR_NAME,
+            feed_dict={self.INPUT_TENSOR_NAME: [np.asarray(resized_image)]})
+        seg_map = batch_seg_map[0]
+        return resized_image, seg_map
 
 
 def create_pascal_label_colormap():
-  """Creates a label colormap used in PASCAL VOC segmentation benchmark.
+    """Creates a label colormap used in PASCAL VOC segmentation benchmark.
 
-  Returns:
-    A Colormap for visualizing segmentation results.
-  """
-  colormap = np.zeros((256, 3), dtype=int)
-  ind = np.arange(256, dtype=int)
+    Returns:
+      A Colormap for visualizing segmentation results.
+    """
+    colormap = np.zeros((256, 3), dtype=int)
+    ind = np.arange(256, dtype=int)
 
-  for shift in reversed(range(8)):
-    for channel in range(3):
-      colormap[:, channel] |= ((ind >> channel) & 1) << shift
-    ind >>= 3
+    for shift in reversed(range(8)):
+        for channel in range(3):
+            colormap[:, channel] |= ((ind >> channel) & 1) << shift
+        ind >>= 3
 
-  return colormap
+    return colormap
 
 
 def label_to_color_image(label):
-  """Adds color defined by the dataset colormap to the label.
+    """Adds color defined by the dataset colormap to the label.
 
-  Args:
-    label: A 2D array with integer type, storing the segmentation label.
+    Args:
+      label: A 2D array with integer type, storing the segmentation label.
 
-  Returns:
-    result: A 2D array with floating type. The element of the array
-      is the color indexed by the corresponding element in the input label
-      to the PASCAL color map.
+    Returns:
+      result: A 2D array with floating type. The element of the array
+        is the color indexed by the corresponding element in the input label
+        to the PASCAL color map.
 
-  Raises:
-    ValueError: If label is not of rank 2 or its value is larger than color
-      map maximum entry.
-  """
-  if label.ndim != 2:
-    raise ValueError('Expect 2-D input label')
+    Raises:
+      ValueError: If label is not of rank 2 or its value is larger than color
+        map maximum entry.
+    """
+    if label.ndim != 2:
+        raise ValueError('Expect 2-D input label')
 
-  colormap = create_pascal_label_colormap()
+    colormap = create_pascal_label_colormap()
 
-  if np.max(label) >= len(colormap):
-    raise ValueError('label value too large.')
+    if np.max(label) >= len(colormap):
+        raise ValueError('label value too large.')
 
-  return colormap[label]
+    return colormap[label]
 
 
 def vis_segmentation(image, seg_map):
-  """Visualizes input image, segmentation map and overlay view."""
-  plt.figure(figsize=(15, 5))
-  grid_spec = gridspec.GridSpec(1, 4, width_ratios=[6, 6, 6, 1])
+    """Visualizes input image, segmentation map and overlay view."""
+    plt.figure(figsize=(15, 5))
+    grid_spec = gridspec.GridSpec(1, 4, width_ratios=[6, 6, 6, 1])
 
-  plt.subplot(grid_spec[0])
-  plt.imshow(image)
-  plt.axis('off')
-  plt.title('input image')
+    plt.subplot(grid_spec[0])
+    plt.imshow(image)
+    plt.axis('off')
+    plt.title('input image')
 
-  plt.subplot(grid_spec[1])
-  seg_image = label_to_color_image(seg_map).astype(np.uint8)
-  plt.imshow(seg_image)
-  plt.axis('off')
-  plt.title('segmentation map')
+    plt.subplot(grid_spec[1])
+    seg_image = label_to_color_image(seg_map).astype(np.uint8)
+    plt.imshow(seg_image)
+    plt.axis('off')
+    plt.title('segmentation map')
 
-  plt.subplot(grid_spec[2])
-  plt.imshow(image)
-  plt.imshow(seg_image, alpha=0.7)
-  plt.axis('off')
-  plt.title('segmentation overlay')
+    plt.subplot(grid_spec[2])
+    plt.imshow(image)
+    plt.imshow(seg_image, alpha=0.7)
+    plt.axis('off')
+    plt.title('segmentation overlay')
 
-  unique_labels = np.unique(seg_map)
-  ax = plt.subplot(grid_spec[3])
-  plt.imshow(
-      FULL_COLOR_MAP[unique_labels].astype(np.uint8), interpolation='nearest')
-  ax.yaxis.tick_right()
-  plt.yticks(range(len(unique_labels)), LABEL_NAMES[unique_labels])
-  plt.xticks([], [])
-  ax.tick_params(width=0.0)
-  plt.grid('off')
-  plt.show()
+    unique_labels = np.unique(seg_map)
+    ax = plt.subplot(grid_spec[3])
+    plt.imshow(
+        FULL_COLOR_MAP[unique_labels].astype(np.uint8), interpolation='nearest')
+    ax.yaxis.tick_right()
+    plt.yticks(range(len(unique_labels)), LABEL_NAMES[unique_labels])
+    plt.xticks([], [])
+    ax.tick_params(width=0.0)
+    plt.grid('off')
+    plt.show()
 
 
-LABEL_NAMES = np.asarray([
-    'background', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
-    'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike',
-    'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tv'
-])
+def preprocess():
 
-FULL_LABEL_MAP = np.arange(len(LABEL_NAMES)).reshape(len(LABEL_NAMES), 1)
-FULL_COLOR_MAP = label_to_color_image(FULL_LABEL_MAP)
+    LABEL_NAMES = np.asarray([
+        'background', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
+        'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike',
+        'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tv'
+    ])
 
-"""## Select a pretrained model
-We have trained the DeepLab model using various backbone networks. Select one from the MODEL_NAME list.
-"""
+    FULL_LABEL_MAP = np.arange(len(LABEL_NAMES)).reshape(len(LABEL_NAMES), 1)
+    FULL_COLOR_MAP = label_to_color_image(FULL_LABEL_MAP)
 
-MODEL_NAME = 'mobilenetv2_coco_voctrainaug'  # @param ['mobilenetv2_coco_voctrainaug', 'mobilenetv2_coco_voctrainval', 'xception_coco_voctrainaug', 'xception_coco_voctrainval']
+    """## Select a pretrained model
+    We have trained the DeepLab model using various backbone networks. Select one from the MODEL_NAME list.
+    """
 
-_DOWNLOAD_URL_PREFIX = 'http://download.tensorflow.org/models/'
-_MODEL_URLS = {
-    'mobilenetv2_coco_voctrainaug':
-        'deeplabv3_mnv2_pascal_train_aug_2018_01_29.tar.gz',
-    'mobilenetv2_coco_voctrainval':
-        'deeplabv3_mnv2_pascal_trainval_2018_01_29.tar.gz',
-    'xception_coco_voctrainaug':
-        'deeplabv3_pascal_train_aug_2018_01_04.tar.gz',
-    'xception_coco_voctrainval':
-        'deeplabv3_pascal_trainval_2018_01_04.tar.gz',
-}
-_TARBALL_NAME = 'deeplab_model.tar.gz'
+    # @param ['mobilenetv2_coco_voctrainaug', 'mobilenetv2_coco_voctrainval', 'xception_coco_voctrainaug', 'xception_coco_voctrainval']
+    MODEL_NAME = 'mobilenetv2_coco_voctrainaug'
 
-model_dir = tempfile.mkdtemp()
-tf.io.gfile.makedirs(model_dir)
-#tf.gfile.MakeDirs(model_dir)
+    _DOWNLOAD_URL_PREFIX = 'http://download.tensorflow.org/models/'
+    _MODEL_URLS = {
+        'mobilenetv2_coco_voctrainaug':
+            'deeplabv3_mnv2_pascal_train_aug_2018_01_29.tar.gz',
+        'mobilenetv2_coco_voctrainval':
+            'deeplabv3_mnv2_pascal_trainval_2018_01_29.tar.gz',
+        'xception_coco_voctrainaug':
+            'deeplabv3_pascal_train_aug_2018_01_04.tar.gz',
+        'xception_coco_voctrainval':
+            'deeplabv3_pascal_trainval_2018_01_04.tar.gz',
+    }
+    _TARBALL_NAME = 'deeplab_model.tar.gz'
 
-download_path = os.path.join(model_dir, _TARBALL_NAME)
-print('downloading model, this might take a while...')
-urllib.request.urlretrieve(_DOWNLOAD_URL_PREFIX + _MODEL_URLS[MODEL_NAME],
-                   download_path)
-print('download completed! loading DeepLab model...')
+    model_dir = tempfile.mkdtemp()
+    tf.io.gfile.makedirs(model_dir)
+    # tf.gfile.MakeDirs(model_dir)
 
-MODEL = DeepLabModel(download_path)
-print('model loaded successfully!')
+    download_path = os.path.join(model_dir, _TARBALL_NAME)
+    print('downloading model, this might take a while...')
+    urllib.request.urlretrieve(_DOWNLOAD_URL_PREFIX + _MODEL_URLS[MODEL_NAME],
+                            download_path)
+    print('download completed! loading DeepLab model...')
 
-"""## Run on sample images
+    MODEL = DeepLabModel(download_path)
+    print('model loaded successfully!')
 
-Select one of sample images (leave `IMAGE_URL` empty) or feed any internet image
-url for inference.
+    """## Run on sample images
 
-Note that this colab uses single scale inference for fast computation,
-so the results may slightly differ from the visualizations in the
-[README](https://github.com/tensorflow/models/blob/master/research/deeplab/README.md) file,
-which uses multi-scale and left-right flipped inputs.
-"""
+    Select one of sample images (leave `IMAGE_URL` empty) or feed any internet image
+    url for inference.
 
-SAMPLE_IMAGE = 'image1'  # @param ['image1', 'image2', 'image3']
-IMAGE_URL = ''  #@param {type:"string"}
+    Note that this colab uses single scale inference for fast computation,
+    so the results may slightly differ from the visualizations in the
+    [README](https://github.com/tensorflow/models/blob/master/research/deeplab/README.md) file,
+    which uses multi-scale and left-right flipped inputs.
+    """
 
-_SAMPLE_URL = ('https://github.com/tensorflow/models/blob/master/research/'
-               'deeplab/g3doc/img/%s.jpg?raw=true')
+    SAMPLE_IMAGE = 'image1'  # @param ['image1', 'image2', 'image3']
+    IMAGE_URL = ''  # @param {type:"string"}
+
+    _SAMPLE_URL = ('https://github.com/tensorflow/models/blob/master/research/'
+                'deeplab/g3doc/img/%s.jpg?raw=true')
+
+    image_url = IMAGE_URL or _SAMPLE_URL % SAMPLE_IMAGE
+    run_visualization(image_url)
 
 
 def run_visualization(url):
-  """Inferences DeepLab model and visualizes result."""
-  try:
-    f = urllib.request.urlopen(url)
-    jpeg_str = f.read()
-    original_im = Image.open(BytesIO(jpeg_str))
-  except IOError:
-    print('Cannot retrieve image. Please check url: ' + url)
-    return
+    """Inferences DeepLab model and visualizes result."""
+    try:
+        f = urllib.request.urlopen(url)
+        jpeg_str = f.read()
+        original_im = Image.open(BytesIO(jpeg_str))
+    except IOError:
+        print('Cannot retrieve image. Please check url: ' + url)
+        return
 
-  print('running deeplab on image %s...' % url)
-  resized_im, seg_map = MODEL.run(original_im)
+    print('running deeplab on image %s...' % url)
+    resized_im, seg_map = MODEL.run(original_im)
 
-  vis_segmentation(resized_im, seg_map)
+    vis_segmentation(resized_im, seg_map)
 
-
-image_url = IMAGE_URL or _SAMPLE_URL % SAMPLE_IMAGE
-run_visualization(image_url)
+if __name__ == "__main__":
+    try:
+        preprocess()
+    except Exception as e:
+        print(e)
 
 """## What's next
 
