0aa428405128ed824fb7fe98a29093f060fb2915
Ashwin Phadke <ashwinphadke12@rediffmail.com>
2020-09-20 22:33:08 +0530

    - added function for pose estimation.

--- cvplay/cvplay/run_video_pose_estimation.py
+++ cvplay/cvplay/run_video_pose_estimation.py
@@ -19,13 +19,20 @@ logger.addHandler(ch)
 fps_time = 0
 
 
-if __name__ == '__main__':
+def estimate_pose(id, video, model):
+
+    VID_SAVE_PATH = 'static/'
+    BASE_PATH = 'models/'
+    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+    out = cv2.VideoWriter(VID_SAVE_PATH + id + '.mp4',
+                        fourcc, 20.0, (640, 480))
+
     parser = argparse.ArgumentParser(description='tf-pose-estimation Video')
     parser.add_argument('--video', type=str, default='')
     parser.add_argument('--resolution', type=str, default='432x368', help='network input resolution. default=432x368')
     parser.add_argument('--model', type=str, default='mobilenet_thin', help='cmu / mobilenet_thin / mobilenet_v2_large / mobilenet_v2_small')
-    parser.add_argument('--show-process', type=bool, default=False,
-                        help='for debug purpose, if enabled, speed for inference is dropped.')
+    # parser.add_argument('--show-process', type=bool, default=False,
+    #                     help='for debug purpose, if enabled, speed for inference is dropped.')
     parser.add_argument('--showBG', type=bool, default=True, help='False to show skeleton only.')
     args = parser.parse_args()
 
@@ -46,10 +53,18 @@ if __name__ == '__main__':
 
         cv2.putText(image, "FPS: %f" % (1.0 / (time.time() - fps_time)), (10, 10),  cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
         resized = cv2.resize(image, (640,480))
+        out.write(resized)
         cv2.imshow('tf-pose-estimation result', resized)
         fps_time = time.time()
         if cv2.waitKey(1) == 27:
             break
-
+    cap.release()
+    out.release()
     cv2.destroyAllWindows()
 logger.debug('finished+')
+
+
+
+
+if __name__ == '__main__':
+    pass()
