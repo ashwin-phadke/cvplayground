094c1089346b63eed9340991adaf340727d3eb8f
Ashwin Phadke <ashwinphadke12@rediffmail.com>
2020-11-01 21:42:24 +0530

    - added vidsegmentation process flow.

--- cvplay/cvplay/flask_process.py
+++ cvplay/cvplay/flask_process.py
@@ -47,6 +47,23 @@ def process_segment_image():
     conn.close()
     return id
 
+def process_segment_video():
+    conn = sqlite3.connect(
+        'db/cvplayground.sqlite')
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT id, location, model_name FROM uploads WHERE isProcessed=0 order by datetime DESC")
+    #cur.execute("SELECT id, location, model_name FROM uploads WHERE isProcessed=0 order by datetime DESC LIMIT 1")
+    id, location, model_name = cur.fetchone()
+    if not (id, location):
+        cur.execute(
+            "SELECT id, location FROM uploads WHERE isProcessed=0 order by datetime DESC")
+    preprocess(location, id, model_name)
+    cur.execute("UPDATE uploads SET isProcessed=1  WHERE id='"+id+"'")
+    conn.commit()
+    conn.close()
+    return id
+
 
 def process_objdet_image():
     conn = sqlite3.connect(
